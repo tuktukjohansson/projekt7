1ec8ada2b38552eaddb1e66048dd938df64a39d0
Strejve <luie_540@hotmail.com>
2021-10-12 15:00:12 +0200

    "En enkel CreateBOActivity.kt skapad, samt en klass för BussinessOwner skapad"

---
+++ app/src/main/java/com/example/projekt7/BusinessOwner.kt
@@ -0,0 +1,5 @@
+package com.example.projekt7
+
+class BusinessOwner (name: String, business : String, adressStreet: String, townZipCode: String, phone: Int,
+                    mail: String, password: String, x: Int, y:Int) {
+}
---
+++ app/src/main/java/com/example/projekt7/RegularUser.kt
@@ -0,0 +1,4 @@
+package com.example.projekt7
+
+class RegularUser {
+}
