abdc2215d5e7ebff589c470cb2cc531165f5ac07
Strejve <luie_540@hotmail.com>
2021-10-13 18:23:39 +0200

    Jag har fixat en DataManager med lista av regularUsers. I aktiviteten CreateRUActivity.kt lägger man in ny user i listan.

---
+++ app/src/main/java/com/example/projekt7/DataManager.kt
@@ -0,0 +1,6 @@
+package com.example.projekt7
+
+object DataManager {
+    var regularUserList = mutableListOf<RegularUser>()
+
+}
--- app/src/main/java/com/example/projekt7/RegularUser.kt
+++ app/src/main/java/com/example/projekt7/RegularUser.kt
@@ -1,4 +1,4 @@
 package com.example.projekt7
 
-class RegularUser (name:String, mail: String, userName: String, password: String) {
+class RegularUser (var name:String,var  mail: String, var userName: String,var password: String) {
 }
