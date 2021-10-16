342ef786a9e0f7bc37dd27023c167b15cf255feb
IsmeniaM <brandaoismenia@gmail.com>
2021-10-16 21:40:54 +0200

    Map Activity created.

---
+++ app/src/main/java/com/example/projekt7/MapActivity.kt
@@ -0,0 +1,13 @@
+package com.example.projekt7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+
+
+    }
+}
