ef03f75160e2523d32b0888880fdb16f87105f6f
IsmeniaM <brandaoismenia@gmail.com>
2021-10-19 16:37:22 +0200

    Jag har skapat 3 lokaler.

--- app/src/main/java/com/example/projekt7/RUMapActivity.kt
+++ app/src/main/java/com/example/projekt7/RUMapActivity.kt
@@ -10,6 +10,7 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.projekt7.databinding.ActivityRumapBinding
+import com.example.projekt7.user_profile_fragments.PlacesInfoAdapter
 
 class RUMapActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -40,9 +41,45 @@ class RUMapActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val adapter = PlacesInfoAdapter(this)
+        mMap.setInfoWindowAdapter(adapter)
+
+        createMarkers()
+    }
+
+    fun createMarkers() {
+
+        val abCafe = LatLng (59.2995615, 17.9985487)
+        val marker = mMap.addMarker(MarkerOptions()
+            .position(abCafe)
+            .title("A.B.Café")
+            .snippet("Roger")
+        )
+
+        marker.tag = R.drawable.ic_baseline_stars_24
+
+
+        val icaSup = LatLng (59.2968838, 17.9830697)
+        val marker2 = mMap.addMarker(MarkerOptions()
+            .position(icaSup)
+            .title("ICA Supermarket")
+            .snippet("Anabella")
+        )
+
+        marker2.tag = R.drawable.ic_baseline_stars_24
+
+
+        val pizzeria = LatLng (59.36634, 16.50773)
+        val marker3 = mMap.addMarker(MarkerOptions()
+            .position(pizzeria)
+            .title("Järntorget Pizzeria")
+            .snippet("Robert")
+        )
+
+        marker3.tag = R.drawable.ic_baseline_stars_24
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/projekt7/user_profile_fragments/PlacesInfoAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.projekt7.user_profile_fragments
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.projekt7.R
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.Marker
+
+class PlacesInfoAdapter ( val context: Context) : GoogleMap.InfoWindowAdapter {
+
+    val layoutInflater = LayoutInflater.from(context)
+
+    override fun getInfoWindow(marker: Marker): View? {
+        val infoWindow = layoutInflater.inflate(R.layout.info_window_ru,null)
+
+        val imageView = infoWindow.findViewById<ImageView>(R.id.image_star)
+        val titleView = infoWindow.findViewById<TextView>(R.id.local_name)
+        val userView = infoWindow.findViewById<TextView>(R.id.user_username)
+
+        titleView.text = marker.title
+        userView.text = marker.snippet
+        imageView.setImageResource(marker.tag as Int)
+
+        return infoWindow
+    }
+
+    override fun getInfoContents(marker: Marker): View? {
+
+
+        return null
+    }
+
+
+}
