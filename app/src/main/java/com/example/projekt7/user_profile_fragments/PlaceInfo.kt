f6068d987686595e45917ee271e709a5401aae8e
IsmeniaM <brandaoismenia@gmail.com>
2021-10-19 23:01:43 +0200

    New bransh av RUMap. På den här branshen har jag fixat Zoom, new places, och få tillgång till användarens position.

--- app/src/main/java/com/example/projekt7/user_profile_fragments/PlaceInfo.kt
+++ app/src/main/java/com/example/projekt7/user_profile_fragments/PlaceInfo.kt
@@ -1,8 +1,10 @@
 package com.example.projekt7.user_profile_fragments
 
+import com.google.android.gms.maps.model.LatLng
+
 class PlaceInfo(
     val name: String,
     val ruName: String,
-    val position: com.google.android.gms.maps.model.LatLng,
+    val position: LatLng,
     val image: Int) {
 }
