cd6154439fddff3d7e00af362445266b66a9958f
Lagos <social_a@live.com>
2021-11-03 14:47:41 +0100

    Adding possibility to upload image on dialog screen

--- app/src/main/java/com/example/projekt7/UploadImageActivity.kt
+++ app/src/main/java/com/example/projekt7/UploadImageActivity.kt
@@ -13,25 +13,26 @@ import java.util.*
 
 class UploadImageActivity : AppCompatActivity() {
 
-    lateinit var binding : ActivityUploadImageBinding
-    lateinit var ImageUri : Uri
+//    lateinit var binding : ActivityUploadImageBinding
+//    lateinit var ImageUri : Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityUploadImageBinding.inflate(layoutInflater)
+/*       binding = ActivityUploadImageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnImageSelect.setOnClickListener {
+       binding.btnImageSelect.setOnClickListener {
             selectImage()
 
         }
-
         binding.btnUploadImage.setOnClickListener {
             uploadImage()
 
         }
-    }
 
+ */
+    }
+/*
     private fun uploadImage() {
 
         val progressDialog = ProgressDialog(this)
@@ -74,4 +75,6 @@ class UploadImageActivity : AppCompatActivity() {
 
         }
     }
+    */
+ */
 }
