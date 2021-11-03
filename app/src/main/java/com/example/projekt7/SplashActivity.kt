ca25e24b6ccfb25d9c71a13a92692d133c351af5
Lagos <social_a@live.com>
2021-11-03 23:24:23 +0100

    Splashscreen is added. Works properly.

---
+++ app/src/main/java/com/example/projekt7/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.projekt7
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this,LoginScreenActivity::class.java)
+            startActivity(intent)
+        }, 2500)
+
+    }
+}
