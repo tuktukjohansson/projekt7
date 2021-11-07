c8313a2e1b88d2960a29ed3aee30ed25d888fbf9
Lagos <social_a@live.com>
2021-11-07 15:37:48 +0100

    Fixing adapter

--- app/src/main/java/com/example/projekt7/MapsAdapter.kt
+++ app/src/main/java/com/example/projekt7/MapsAdapter.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.projekt7.Model.UserMap
 
@@ -16,26 +18,27 @@ data class MapsAdapter (val context : Context, val userMaps : List<UserMap>, val
 
     //(R.layout.simple_list_item_1) has been deleted and exchanged
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.item_saved_spots, parent, false)
-//        val view = LayoutInflater.from(context).inflate(R.id.sav, parent, false)
+//        val view = LayoutInflater.from(context).inflate(R.layout.item_saved_spots, parent, false)
+          val view = LayoutInflater.from(context).inflate(R.layout.simple_list_item_1), parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(userMaps[position])
-//        val userMap = userMaps[position]
+          val userMap = userMaps[position]
         holder.itemView.setOnClickListener{
             onClickListener.onItemClick(position)
         }
-//        val textViewTitle = holder.itemView.findViewById<TextView>(android.R.id.text1)
-//        textViewTitle.text = userMap.title
+
+        val textViewTitle = holder.itemView.findViewById<TextView>(android.R.id.text1)
+        textViewTitle.text = userMap.title
     }
 
     override fun getItemCount() = userMaps.size
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        fun bind(userMap: UserMap) {
-        }
-
+/*        val tvUsername : TextView = itemView.findViewById(R.id.tvUsername)
+        val tvDescription : TextView = itemView.findViewById(R.id.tvDescription)
+        val tvImage : ImageView = itemView.findViewById(R.id.ivImage)
+*/
     }
 }
